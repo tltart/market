0faf593da6972edb412dd50de73f5a2bb9efde52
art <art.tlt-art@yandex.ru>
2021-09-08 21:05:59 +0400

    ...

--- client_2/src/components/RegisterForm/registerForm.jsx
+++ client_2/src/components/RegisterForm/registerForm.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { Field, reduxForm, reset } from 'redux-form'
 import InputForm from '../common/inputForm/inputForm'
 import c from './registerForm.module.css'
-import {required} from '../common/Validate/validate'
+import { required } from '../common/Validate/validate'
+import Cc from '../common/Cal/cal'
+
+
 
 let RegisterForm = props => {
   const { handleSubmit, title, isLogin } = props
@@ -10,9 +13,10 @@ let RegisterForm = props => {
     <div className={c.forma__wrapper}>
       <form onSubmit={handleSubmit}>
         <h1>{title}</h1>
-        <Field name='email' type='email' component={InputForm} placeholder="Введите почту" validate={[required]}/>
-        <Field name='password' type='password' component={InputForm} placeholder="Введите пароль" validate={[required]}/>
-        {!isLogin && <Field name='password' type='password' component={InputForm} placeholder="Повторите пароль" validate={[required]}/>}
+        <Field name='email' type='email' component={InputForm} placeholder="Введите почту" validate={[required]} />
+        <Field name='password' type='password' component={InputForm} placeholder="Введите пароль" validate={[required]} />
+        {/* <Field name='date' type='text' component={Cc} placeholder="Дату" validate={[required]} /> */}
+        {!isLogin && <Field name='password' type='password' component={InputForm} placeholder="Повторите пароль" validate={[required]} />}
         <button className={c.button}>Регистрация</button>
       </form>
     </div>
