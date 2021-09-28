e4b75ae6707c9ff6e2d3a011ff6d7d5238c1bf83
Artem <art.artem001@yandex.ru>
2021-09-28 18:31:49 +0400

    ....

--- client_2/src/components/trackCard/trackCard.jsx
+++ client_2/src/components/trackCard/trackCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import c from './trackCard.module.css'
 
-const TrackCard = ({order}) => {
+const TrackCard = ({ order }) => {
 
-    return(
+    return (
         <div>
-            {order.id}
+            <div id={c.card__track}>
+                <hr />
+                <span><h3>Название: {order.name}</h3></span>
+                <span><h3>Вкус: {order.taste}</h3></span>
+                <span><h3>Дата: {order.date}</h3><h4>Осталось: </h4></span>
+                
+                <hr />
+            </div>
+
         </div>
     )
 }
--- client_2/src/pages/track.jsx
+++ client_2/src/pages/track.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { useEffect } from 'react';
 import { GetOrder } from '../store/orderReducer';
 import TrackCard from '../components/trackCard/trackCard';
+import c from './track.module.css'
+import moment from 'moment';
 
 
 const mapStateToProps = (state) => {
@@ -16,18 +18,27 @@ const TrackPage = ({ orders, GetOrder }) => {
 
     // Нуен запрос на сервер. НЕ ЗАБУДЬ!!!! /////////////////////////////////////////////
 
+    // console.log(orders[0].date);
+
+
     const storeGet = () => {
         if (!orders.length) {
             GetOrder();
+            var now = moment(new Date()); //todays date
+            var end = moment(orders[0].date); // another date
+            var duration = moment.duration(now.diff(end));
+            var days = duration.asDays();
+            console.log(days)
         }
     }
 
     useEffect(() => {
         storeGet();
+
     }, [orders]);
 
     return (
-        <div>
+        <div id={c.wrap__card}>
             <h2>Отслеживание заказов</h2>
             {orders.length ? 
                 orders.map(item => <TrackCard key={item.id} order={item}/>)
